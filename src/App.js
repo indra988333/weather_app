8374801d61da7a884f186a2a15910afc8de76841
inj988 <injana10@gmail.com>
2022-10-11 11:40:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import {useEffect,useState} from "react";
 
 function App() {
+  const apiKey="f56f24967aaf51182d1d4df628297c6d"
+  const [data, setData] = useState({})
+  const getWeatherDetails=(cityName)=>{
+    if(!cityName) return
+    const apiURL="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey
+    axios.get(apiURL).then((res)=>{
+      console.log("response",res.data)
+
+      setData(res.data)
+
+    }).catch((err)=>{
+      console.log("err",err)
+    })
+  }
+  const [inputCity, setInputCity]=useState("")
+  
+
+  const handleChangeInput=(e)=>{
+    console.log("value",e.target.value)
+      setInputCity(e.target.value)
+  }
+
+
+
+  const handleSearch=()=>{
+    getWeatherDetails(inputCity)
+  }
+useEffect(()=>{
+  getWeatherDetails("delhi")
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="col-md-12">
+      <div className="weatherBg">
+      <h1 className="head">Weather App</h1>
+      <div className="d-grid gap-4 col-4 mt-4">
+      <input type="text"  className="form-control"
+      value={inputCity}
+      onChange={handleChangeInput}/>
+      <button className="btn btn-primary" type="button" onClick={handleSearch}>
+      Search
+
+      </button>
+      </div>
+      </div>
+
+        <div className="col-md-12 text-center mt-5">
+        <div className="shadow rounded weatherResultBox">
+      <img className="Wicon" src="https://img.freepik.com/premium-vector/cute-weather-icon-set-weather-forecast-icon-isolated-white-background_68708-952.jpg?w=1060"/>
+              <h5 className="wCity">
+                {data?.name}
+              </h5>
+              <h6 className="wTemp">{((data?.main?.temp)-273.15).toFixed(2)}°C</h6>
+        </div>
+      </div>
     </div>
+
   );
 }
 
---
+++ src/components/Component2.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Component2(){
+    return(
+        <div>
+            Indranil
+        </div>
+
+    );
+}
+
+export default Component2
